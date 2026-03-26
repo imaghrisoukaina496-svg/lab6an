e7e7dcc968de7dd8317281e4b7064cc157ecbb66
soukaina <imaghrisoukaina496@gmail.com>
2026-03-26 02:01:08 +0100

    mise a jour du projet

--- app/src/main/java/com/example/pizzarecipes/ui/ListPizzaActivity.java
+++ app/src/main/java/com/example/pizzarecipes/ui/ListPizzaActivity.java
@@ -1,5 +1,6 @@
 package com.example.pizzarecipes.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.*;
@@ -9,8 +10,9 @@ import com.example.pizzarecipes.adapter.PizzaAdapter;
 import com.example.pizzarecipes.classes.Produit;
 import com.example.pizzarecipes.service.ProduitService;
 import java.util.List;
-public class ListPizzaActivity extends AppCompatActivity{
+public class ListPizzaActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
@@ -19,7 +21,13 @@ public class ListPizzaActivity extends AppCompatActivity{
         ListView lv = findViewById(R.id.lvPizzas);
         List<Produit> pizzas = ProduitService.getInstance().findAll();
 
-        lv.setAdapter(new PizzaAdapter(this, pizzas));
+        PizzaAdapter adapter = new PizzaAdapter(this, pizzas);
+        lv.setAdapter(adapter);
+
+        lv.setOnTouchListener((v, event) -> {
+            v.getParent().requestDisallowInterceptTouchEvent(true);
+            return false;
+        });
 
         lv.setOnItemClickListener((parent, view, pos, id) -> {
             Intent it = new Intent(this, PizzaDetailActivity.class);
